6f346410b86924d6034b09aee07f9d4496c4a2be
Kael <kaelanreece@gmail.com>
2020-02-13 20:57:50 +1000

    start work towards login/register users

--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,13 @@ using ConsoleApp.PostgreSQL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace PersonalSite
 {
@@ -17,10 +19,7 @@ namespace PersonalSite
 			Configuration = configuration;
 		}
 
-		public IConfiguration Configuration
-		{
-			get;
-		}
+		public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
@@ -37,44 +36,42 @@ namespace PersonalSite
 			services.AddDbContext<SiteDbContext>(options =>
 				options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
-			// services.AddIdentity<User, IdentityRole<long>>()
-			// 	.AddEntityFrameworkStores<ApplicationDbContext, long>()
-			// 	.AddDefaultTokenProviders();
+			services.AddIdentityCore<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddDefaultTokenProviders()
+				.AddEntityFrameworkStores<SiteDbContext>();
 
-			// services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-			//     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-			// services.Configure<IdentityOptions>(options =>
-			// {
-			// 	// Password settings.
-			// 	options.Password.RequireDigit = true;
-			// 	options.Password.RequireLowercase = true;
-			// 	options.Password.RequireNonAlphanumeric = true;
-			// 	options.Password.RequireUppercase = true;
-			// 	options.Password.RequiredLength = 6;
-			// 	options.Password.RequiredUniqueChars = 1;
+			services.Configure<IdentityOptions>(options =>
+			{
+				 // Password settings.
+				 options.Password.RequireDigit = true;
+				options.Password.RequireLowercase = true;
+				options.Password.RequireNonAlphanumeric = true;
+				options.Password.RequireUppercase = true;
+				options.Password.RequiredLength = 6;
+				options.Password.RequiredUniqueChars = 1;
 
-			// 	// Lockout settings.
-			// 	options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-			// 	options.Lockout.MaxFailedAccessAttempts = 5;
-			// 	options.Lockout.AllowedForNewUsers = true;
+				 // Lockout settings.
+				 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+				options.Lockout.MaxFailedAccessAttempts = 5;
+				options.Lockout.AllowedForNewUsers = true;
 
-			// 	// User settings.
-			// 	options.User.AllowedUserNameCharacters =
-			// 		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-			// 	options.User.RequireUniqueEmail = false;
-			// });
+				 // User settings.
+				 options.User.AllowedUserNameCharacters =
+					  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+				options.User.RequireUniqueEmail = false;
+			});
 
-			// services.ConfigureApplicationCookie(options =>
-			// {
-			// 	// Cookie settings
-			// 	options.Cookie.HttpOnly = true;
-			// 	options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+			services.ConfigureApplicationCookie(options =>
+			{
+				 // Cookie settings
+				 options.Cookie.HttpOnly = true;
+				options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
 
-			// 	options.LoginPath = "/Identity/Account/Login";
-			// 	options.AccessDeniedPath = "/Identity/Account/AccessDenied";
-			// 	options.SlidingExpiration = true;
-			// });
+				options.LoginPath = "/Identity/Account/Login";
+				options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+				options.SlidingExpiration = true;
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -96,6 +93,10 @@ namespace PersonalSite
 			app.UseSpaStaticFiles();
 
 			app.UseRouting();
+
+
+			app.UseAuthentication();
+			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
 			{
