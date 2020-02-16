a3d9e1ba924f98197e4a74241c9ee57b3d1189a6
Kael <kaelanreece@gmail.com>
2020-02-16 15:50:15 +1000

    now redirected to home page on logout

--- ClientApp/src/Components/Pages/Logout.tsx
+++ ClientApp/src/Components/Pages/Logout.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import axios from 'axios';
+import { Redirect } from 'react-router';
 
-export default function Logout() {
-	axios.post('/Identity/Account/Logout')
-		.then(function (response) {
-			console.log(response);
-			//Perform action based on response
-		})
-		.catch(function (error) {
-			console.log(error);
-			//Perform action based on error
-		});
-	return (
-		null
-	)
+export default class Logout extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	LogoutUser = async () => {
+		await axios.post('/Identity/Account/Logout')
+			.then(function (response) {
+				console.log(response);
+				//Perform action based on response
+			})
+			.catch(function (error) {
+				console.log(error);
+				//Perform action based on error
+			})
+	};
+	render() {
+		this.LogoutUser();
+		return (
+			<Redirect to='/' />
+		)
+	}
 }
