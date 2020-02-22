f36a6750de8b71c69955e0a8ba656443a94760f0
Kael <kaelanreece@gmail.com>
2020-02-23 00:27:02 +1000

    add clientside required validation to login

--- ClientApp/src/Components/Pages/Login.tsx
+++ ClientApp/src/Components/Pages/Login.tsx
@@ -1,19 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component, EventHandler } from 'react'
 import axios from 'axios';
 import { store } from '../store';
 
 interface LoginState {
 	Email: string
 	Password: string
+	errorMessage: string
 }
 
 export default class Login extends Component<{}, LoginState> {
 	constructor(props: any) {
 		super(props);
-		this.setState({
+		this.state = {
 			Password: '',
-			Email: ''
-		});
+			Email: '',
+			errorMessage: '',
+		};
 	}
 
 	render() {
@@ -21,23 +23,46 @@ export default class Login extends Component<{}, LoginState> {
 			<>
 				<form onSubmit={this.SubmitHandler}>
 					<h1>Login</h1>
+					<p>{`${this.state.errorMessage}`}</p>
 					<input type="Email" onChange={
-						(e) => this.setState({
-							Email: e.target.value
-						})}
+						(e) => {
+							this.setState({
+								Email: e.target.value
+							})
+							if (this.state.errorMessage != '') {
+								this.ValidateForm();
+							}
+						}}
 					/>
 					<input type="Password" onChange={
-						(e) => this.setState({
-							Password: e.target.value
-						})}
+						(e) => {
+							this.setState({
+								Password: e.target.value
+							})
+							if (this.state.errorMessage != '') {
+								this.ValidateForm();
+							}
+						}}
 					/>
 					<input type="submit" value="Login" />
 				</form>
 			</>
 		)
 	}
+
+	// Validate the form on the clientside.
+	ValidateForm = (): boolean => {
+		if (this.state.Email == '' || this.state.Password == '') {
+			this.setState({ errorMessage: "Email/Password is Required" })
+			return false
+		}
+		this.setState({ errorMessage: '' })
+		return true;
+	}
 	SubmitHandler = (event: any) => {
 		event.preventDefault();
+		if (!this.ValidateForm()) { return; }
+
 		axios.post('/Identity/Account/Login', { "Email": this.state.Email, "Password": this.state.Password })
 			.then(function (response) {
 				console.log(response);
@@ -49,6 +74,5 @@ export default class Login extends Component<{}, LoginState> {
 			.catch(function (error) {
 				console.log(error);
 			});
-
 	}
 }
