e863efe6718db2081917ca90348e61a03c6a3680
Kael <kaelanreece@gmail.com>
2020-02-22 17:38:28 +1000

    add docs to abstract model

--- Models/AbstractModel.cs
+++ Models/AbstractModel.cs
@@ -3,13 +3,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PersonalSite.Models
 {
+	/// <summary>
+	/// All shared model attributes.
+	/// </summary>
 	public abstract class AbstractModel
 	{
+		/// <summary>
+		/// Gets or sets the primary key of the entity.
+		/// </summary>
 		[Key]
-		public Guid Id
-		{
-			get;
-			set;
-		}
+		public Guid Id { get; set; }
 	}
 }
