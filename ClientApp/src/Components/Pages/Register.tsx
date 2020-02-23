c0960de0e29c97e8d3cf353be45d97dc9e12821a
Kaelan <kaelanreece@gmail.com>
2020-02-23 17:18:26 +1000

    initial work toward setting up validation on registration form

--- ClientApp/src/Components/Pages/Register.tsx
+++ ClientApp/src/Components/Pages/Register.tsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import ErrorMessage from '../error';
 
-export default class Register extends Component {
+interface LoginState {
+	Email: string
+	Password: string
+	ConfirmPassword: string
+	Username: string
+	errorMessage: string
+}
+
+export default class Register extends Component<{}, LoginState> {
+	constructor(props:any) {
+		super(props);
+		this.state = {
+			Email: '',
+			Password: '',
+			ConfirmPassword: '',
+			Username: '',
+			errorMessage: '',
+		}
+	}
 	render() {
 		return (
 			<>
 				<h1>Register</h1>
-				<form>
+				<ErrorMessage>{this.state.errorMessage}</ErrorMessage>
+				<form onSubmit={this.SubmitHandler}> 
 					<span>Email</span>
 					<input type="text" />
 					<span>Username</span>
@@ -19,4 +39,17 @@ export default class Register extends Component {
 			</>
 		)
 	}
+	// Validate the form on the clientside.
+	ValidateForm = (): boolean => {
+		if (this.state.Email == '' || this.state.Password == '') {
+			this.setState({ errorMessage: "Email/Password is Required" })
+			return false
+		}
+		this.setState({ errorMessage: '' })
+		return true;
+	}
+	SubmitHandler = (event: any) => {
+		event.preventDefault();
+		if (!this.ValidateForm()) { return; }
+	}	
 }
