2db0c983c220880d2387dbc8751e75beeb84858d
Kael <kaelanreece@gmail.com>
2020-02-23 10:33:58 +1000

    automatically set email in contact form if the user is logged in

--- ClientApp/src/Components/Pages/Contact.tsx
+++ ClientApp/src/Components/Pages/Contact.tsx
@@ -1,13 +1,17 @@
 ﻿import React, { Component, PureComponent } from 'react'
-
+import { store } from '../store'
 export default class Contact extends PureComponent {
 	render() {
+		let defaultEmail = '';
+		if (store.checkUserData.email) {
+			defaultEmail = store.checkUserData.email
+		}
 		return (
 			<form>
 				<h1>Contact Me</h1>
 				<p>If you would like to contact me directly, please include your name, email and what you want to talk about in the fields below</p>
 				<p>email</p>
-				<input type="email" />
+				<input type="email" value={defaultEmail} />
 				<p>Message</p>
 				<textarea />
 				<input type="submit" />
