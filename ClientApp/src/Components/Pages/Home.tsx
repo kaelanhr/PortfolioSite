1f975ef56c6690dbc2e7b354438968a16a6168ac
Kael <kaelanreece@gmail.com>
2020-02-29 18:02:11 +1000

    began redesigning hte nav bar

--- ClientApp/src/Components/Pages/Home.tsx
+++ ClientApp/src/Components/Pages/Home.tsx
@@ -2,13 +2,8 @@ import React from 'react'
 
 export default function Home() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>My name is Kaelan Reece</p>
-				<p>
-					Welcome to my website
-				</p>
-			</header>
+		<div className="Home">
+			<h1>Kaelan Reece, Software Engineer</h1>
 		</div>
 	)
 }
